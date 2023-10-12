089ab1096511eb48e2445131c28cc52704d4f525
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-10-12 16:41:58 +0700

    Update data.py

--- zika/split/data.py
+++ zika/split/data.py
@@ -4,7 +4,7 @@ from config import CMD_HANDLER as cmd
 class Data:
 
     text_help_menu = (
-        f"**Help for ZikaRobot**\n Prefix: ."
+        f"**Help for sɪᴀʀᴀʙ-ᴜsᴇʀʙᴏᴛ**\n Prefix: ."
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
